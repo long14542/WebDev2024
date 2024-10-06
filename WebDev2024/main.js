6826be54abfcb23eddf5df590cbf6af41b4765b6
long14542 <121614259+long14542@users.noreply.github.com>
2024-10-06 23:42:28 +0700

    fix new arrival

--- WebDev2024/main.js
+++ WebDev2024/main.js
@@ -10,12 +10,42 @@ var swiper = new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+        renderBullet: function (className) {
+            if (index < 5) {
+                return '<span class="' + className + '"></span>';
+            }
+            return ''; 
+        },
     },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    speed: 500, // Thay đổi tốc độ chuyển động của slider
+    speed: 500,
+    on: {
+        slideChange: function () {
+
+            if (this.activeIndex === 0) {
+                this.navigation.$nextEl.addClass('swiper-button-disabled'); 
+            } else {
+                this.navigation.$nextEl.removeClass('swiper-button-disabled'); 
+            }
+
+            if (this.activeIndex === 4) {
+                this.navigation.$prevEl.addClass('swiper-button-disabled'); 
+            } else {
+                this.navigation.$prevEl.removeClass('swiper-button-disabled'); 
+            }
+
+            if (this.activeIndex === 4) {
+                this.autoplay.stop();
+                setTimeout(() => {
+                    this.slideTo(0); 
+                    this.autoplay.start(); 
+                }, 3000); 
+            }
+        },
+    },
 });
 
   
