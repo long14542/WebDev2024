de7cbe2bf443662f57877d0ca4a6aa651dc8ff60
long14542 <121614259+long14542@users.noreply.github.com>
2024-10-08 17:38:13 +0700

    fetch navbar

--- WebDev2024/main.js
+++ WebDev2024/main.js
@@ -22,30 +22,7 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
     speed: 500,
-    on: {
-        slideChange: function () {
-
-            if (this.activeIndex === 0) {
-                this.navigation.$nextEl.addClass('swiper-button-disabled'); 
-            } else {
-                this.navigation.$nextEl.removeClass('swiper-button-disabled'); 
-            }
-
-            if (this.activeIndex === 4) {
-                this.navigation.$prevEl.addClass('swiper-button-disabled'); 
-            } else {
-                this.navigation.$prevEl.removeClass('swiper-button-disabled'); 
-            }
-
-            if (this.activeIndex === 4) {
-                this.autoplay.stop();
-                setTimeout(() => {
-                    this.slideTo(0); 
-                    this.autoplay.start(); 
-                }, 3000); 
-            }
-        },
-    },
+    
 });
 // Swiper for Reviews
 var swiperReviews = new Swiper(".reviews-container", {
